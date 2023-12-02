f53cfbd36eb211c348d586eb530f705c11d9866a
Tvoi_config <kolliceplay220@gmail.com>
2023-12-02 21:41:37 +0300

    release

--- core/models.py
+++ core/models.py
@@ -13,3 +13,30 @@ class Menu(models.Model):
 
     def __str__(self):
         return f'Название товара {self.title}'
+
+class record(models.Model):
+    name = models.CharField(max_length=150, verbose_name='ФИО')
+    number = models.CharField(max_length=150, verbose_name='Номер телефона')
+    email = models.CharField(max_length=150, verbose_name='Электронная почта')
+    
+    class Meta:
+#        managed = False
+        db_table = 'record'
+        verbose_name = 'запись'
+        verbose_name_plural = 'Записи'
+        
+    def __str__(self):
+        return self.name
+
+class completedrecord(models.Model):
+    record = models.ForeignKey(record, on_delete=models.CASCADE, null=True, blank=True,verbose_name = 'Выберите клиента')
+    completed_work = models.TextField('Назначенное время и доп. условия', default="")
+
+    class Meta:
+#        managed = False
+        db_table = 'completedrecord'
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи Выполненые'
+        
+    def __str__(self):
+        return self.completed_work
